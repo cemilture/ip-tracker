3a8f220f2c8a624bed40bd27fa26f30812613f55
cemilture <cemilture@gmail.com>
2024-02-23 16:18:00 +0300

    error handling

--- src/IPTracker.tsx
+++ src/IPTracker.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+interface LocationData {
+  ip: string;
+  city: string;
+  region: string;
+  country_name: string;
+}
+
 const IPTracker: React.FC = () => {
   const [ipAddress, setIpAddress] = useState("");
-  const [locationData, setLocationData] = useState<any | null>(null);
+  const [locationData, setLocationData] = useState<LocationData | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const trackIP = async () => {
     try {
@@ -11,9 +19,31 @@ const IPTracker: React.FC = () => {
       setLocationData(response.data);
       console.log("response", response.data);
     } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(
+          error.response
+            ? `Error: ${error.response.status}`
+            : `No response received: ${error.message}`
+        );
+      } else {
+        setError(`Error fetching location data: ${error}`);
+      }
       console.error("Error fetching location data:", error);
     }
   };
+
+  //   const trackIP = async () => {
+  //     try {
+  //       const response = await fetch(`https://ipapi.co/${ipAddress}/json/`);
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //       const data = await response.json();
+  //       setLocationData(data);
+  //     } catch (error) {
+  //       setError(`Error fetching location data: ${error.message}`);
+  //     }
+  //   };
 
   return (
     <div>
@@ -24,6 +54,8 @@ const IPTracker: React.FC = () => {
         onChange={(e) => setIpAddress(e.target.value)}
       />
       <button onClick={trackIP}>Track IP</button>
+
+      {error && <div style={{ color: "red" }}>{error}</div>}
 
       {locationData && (
         <div>
